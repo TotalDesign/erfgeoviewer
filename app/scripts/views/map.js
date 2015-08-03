1aeba433523028745f4996bbdc6af6830d3479a3
Jason Yergeau <jyergeau@totalactivemedia.nl>
2015-08-03 12:22:59 +0200

    Remove PDOK-specific code

--- app/scripts/views/map.js
+++ app/scripts/views/map.js
@@ -65,22 +65,7 @@ define(["backbone", "backbone.marionette", "leaflet", "d3", "communicator", "con
           self.baseLayer = L.mapbox.tileLayer( tile.id ).addTo( self.map );
           self.state.set('mapUri', tile.id );
         } else {
-          self.map.remove();
-          var RD = new L.Proj.CRS.TMS(
-            'EPSG:28992',
-            '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs',
-            [-285401.92,22598.08,595401.9199999999,903401.9199999999], {
-              resolutions: [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420]
-            });
-          self.map = new L.Map(self.mapboxContainer, {
-            continuousWorld: true,
-            crs: RD,
-            layers: [
-              new L.TileLayer(tile.tilejson.tiles[0], tile.tilejson)
-            ],
-            center: new L.LatLng(52, 5.3),
-            zoom: 8
-          });
+          self.baseLayer = L.mapbox.tileLayer( tile.tilejson ).addTo( self.map );
           self.state.set('mapUri', tile.tilejson );
         }
       });
