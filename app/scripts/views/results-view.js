f16046431ffb38ece9fd9972967ac18c11eb9cb0
Jason Yergeau <jyergeau@totalactivemedia.nl>
2015-10-02 15:52:34 +0200

    show geometeries with search results

--- app/scripts/views/results-view.js
+++ app/scripts/views/results-view.js
@@ -1,16 +1,47 @@
 /**
  * CollectionView for displaying search results.
  */
-define( ["backbone", 'backbone.marionette', "communicator", "materialize.cards", "jquery",
+define( ["backbone", 'backbone.marionette', "communicator", "materialize.cards", "jquery", "leaflet",
          "tpl!template/results.html"],
-  function(Backbone, Marionette, Communicator, Materialize, $,
+  function(Backbone, Marionette, Communicator, Materialize, $, L,
            ResultItemTemplate) {
 
+    var map;
+    var layerGroup;
+
     var ResultItemView = Marionette.ItemView.extend({
+
+      marker: null,
+
+      onDestroy: function() {
+        layerGroup.removeLayer(this.marker);
+      },
+
+      onShow: function() {
+        var p = new L.latLng(
+          this.model.get( 'latitude' ),
+          this.model.get( 'longitude' )
+        );
+        var m = this.model.convertToGeoJSON();
+        if (m) {
+          this.marker = L.mapbox.featureLayer();
+          this.marker.setGeoJSON( m );
+          layerGroup.addLayer( this.marker );
+        }
+      },
+
       template: ResultItemTemplate
+
     });
 
     return Marionette.CollectionView.extend({
+
+      initialize: function() {
+
+        map = Communicator.reqres.request("getMap");
+        layerGroup = L.layerGroup().addTo(map);
+
+      },
 
       events: {
         "click .add-marker": function(e) {
