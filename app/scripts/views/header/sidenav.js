31d6ee9d6039d2836f97948595fe944814ec6d28
Maarten van Dillen <mvandillen@gmail.com>
2015-11-17 14:47:26 +0100

    fixed tab index for main menu items. ref #8797

--- app/scripts/views/header/sidenav.js
+++ app/scripts/views/header/sidenav.js
@@ -14,14 +14,38 @@ define( ['backbone', 'backbone.marionette', 'models/state', 'models/sidenav', 'm
 
       initialize: function() {
         this.collection.on('change add remove', this.render, this);
+
+        //event handlers to update tabindex on main menu close
+        $(document).on("click", "#menu-button", this.updateTabindices);
+        $(document).on("click", "drag-target", this.updateTabindices);
+        $(document).on("click", "#sidenav-overlay", this.updateTabindices);
+      },
+
+      events: {
+        "click #menu-button": function(e) {
+          console.log("menu open");
+        }
       },
 
       onShow: function() {
-        $( '#menu-button' ).sideNav( {
+        var menuButton = $("#menu-button");
+        menuButton.sideNav( {
           edge: 'left',
           menuWidth: 300,
           closeOnClick: true
         } );
+        //event handler to update tabindex on main menu close
+        $("#"+ menuButton.attr('data-activates')).on("click.itemclick", "a:not(.collapsible-header)", this.updateTabindices);
+      },
+
+      updateTabindices: function() {
+        if ($('body').css('overflow') === "hidden") {
+          //menu is open, enable tabbing into the menu items
+          $(".side-nav li a").attr("tabindex", 0);
+        } else {
+          //menu is closed, disable tabbing into the menu items
+          $(".side-nav li a").attr("tabindex", -1);
+        }
       },
 
       serializeData: function() {
